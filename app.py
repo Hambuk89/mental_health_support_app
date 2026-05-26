667dbf44a3436bbce1fb41bdaede1c05092971aa
Han_Yoo <poxsstar@gmail.com>
2026-05-27 10:02:57 +1200

    updated by Han

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Page routes
+# Page routes for the mental health support website (created by Han) #
 @app.route('/')
 def home():
     return render_template('home.html')
