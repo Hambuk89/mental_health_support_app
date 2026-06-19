164d6601c158a9bba238a50a9cbcfd6c7f55e578
Han_Yoo <poxsstar@gmail.com>
2026-06-19 23:57:24 +1200

    Unified mobile layout alignment by Han

--- models.py
+++ models.py
@@ -3,6 +3,9 @@ from extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    contact = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
